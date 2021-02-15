6974ae5e8dbda01c283a5e7b51de0d664f2fe758
Vadim Percov <48429454+bandoto@users.noreply.github.com>
2021-02-15 18:12:31 +0200

    s

--- prog-portf/js/script.js
+++ prog-portf/js/script.js
@@ -1,4 +1,4 @@
-jQuery("document").ready(function($) {
+$(document).ready(function() {
 
     var nav = $('.header');
 
@@ -12,10 +12,10 @@ jQuery("document").ready(function($) {
 
 });
 
-var $page = $('html, body');
-$('a[href*="#portfolio"],a[href*="#skills"],a[href*="#featured"],a[href*="#footer"]').click(function() {
+var $page = $('html');
+$('a[href*="#portfolio"],a[href*="#skills"],a[href*="#featured"],a[href*="#projects"],a[href*="#contact"]').click(function() {
     $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+        scrollTop: $($.attr(this, 'href')).offset().top - 70
     }, 400);
     return false;
 });
@@ -25,69 +25,7 @@ $(document).ready(function() {
     $('.header__burger').click(function(event) {
         $('.header__burger').toggleClass('active');
         $('body').toggleClass('lock');
+        $('.menu__body').toggleClass('active');
     });
 });
 
-
-$(document).ready(function(){
-    $("a[href*=#]").on("click", function(e){
-        var anchor = $(this);
-        trigger1 = $('.menu__body'),
- 
- wrapper = $('body');
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top
-        }, 777);
-        e.preventDefault();
-         trigger1.removeClass('active');
-         wrapper.removeClass('lock');
- 
-        return false;
-    });
-});
-
-// Cache selectors
-var lastId,
-    topMenu = $(".menu__body"),
-    topMenuHeight = topMenu.outerHeight()+15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e){
-  var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-  $('html, body').stop().animate({ 
-      scrollTop: offsetTop
-  }, 300);
-  e.preventDefault();
-});
-
-// Bind to scroll
-$(window).scroll(function(){
-   // Get container scroll position
-   var fromTop = $(this).scrollTop()+topMenuHeight;
-   
-   // Get id of current scroll item
-   var cur = scrollItems.map(function(){
-     if ($(this).offset().top < fromTop)
-       return this;
-   });
-   // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
-   
-   if (lastId !== id) {
-       lastId = id;
-       // Set/remove active class
-       menuItems
-         .parent().removeClass("menu__list_active")
-         .end().filter("[href='#"+id+"']").parent().addClass("menu__list_active");
-   }                   
-});
